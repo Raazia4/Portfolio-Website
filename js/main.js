0c16ba4b6fbc227459bf8f15acc9455af2a565b6
Raazia4 <raaziazafar4@gmail.com>
2025-01-16 12:10:52 +0500

    Develop About Section

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Header Scroll --->Navbar background colour wsill filled
+let nav=document.querySelector(".navbar");
+window.onscroll =function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+// nav hide  --->the navbar on small screen will closed itself  when i click
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
